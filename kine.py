98dfb1bc4e47bb8026859436fec955bb38977cd5
thomaswong25520 <thomaswong8321@gmail.com>
2021-05-20 09:21:22 +0200

    scrapped 10pages, TODO:check epaule au top formation

--- kine.py
+++ kine.py
@@ -11,7 +11,7 @@ import time
 from selenium.webdriver.common.keys import Keys
 #Chrome Options
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 #chrome_options.add_argument("--start-maximized")
 #chrome_options.add_argument("--window-size=1920x1080")
 #chrome_options.add_argument("--window-size=1920,1080")
@@ -37,6 +37,8 @@ url = "https://www.doctolib.fr/"
 # driver.find_element_by_id("didomi-notice-disagree-button").click()
 
 looked_skill = "Rééducation de l'épaule"
+
+# looked_skill = "Kinésithérapie du sport"
 practicians_skills_shoulder = []
 
 title = "Masseur-kinésithérapeute"
@@ -66,12 +68,6 @@ confirm_paris.click()
 enter = driver.find_element_by_xpath('//*[@id="doctor_search_bar"]/form/div[2]/button/span/span')
 enter.click()
 
-height = driver.execute_script("return document.body.scrollHeight")
-    # Scroll to load all results
-for i in range(0,height-1000,500):
-    driver.execute_script("window.scrollTo(0, {});".format(i))
-    time.sleep(0.2) # slow down scrolling to allow results to load
-
 # list_of_practicians = driver.find_elements_by_class_name("dl-button-primary dl-button js-search-result-path")
 text_fields = driver.find_elements_by_xpath('//*[@id="autocomplete-default"]')
 
@@ -94,18 +90,92 @@ list_of_practicians_xpath = ["/html/body/div[7]/div/div[1]/div[1]/div[1]/div[3]/
                              "/html/body/div[7]/div/div[1]/div[10]/div[1]/div[3]/a"]
 
 
-practician_1_xpath = list_of_practicians_xpath[-1]
-print(f"practician: {practician_1_xpath}")
-practician = driver.find_element_by_xpath(practician_1_xpath)
-practician.click()
+height = driver.execute_script("return document.body.scrollHeight")
+# Scroll to load all results
+# for i in range(0,height-1000,500):
+#     driver.execute_script("window.scrollTo(0, {});".format(i))
+#     time.sleep(0.2) # slow down scrolling to allow results to load
 
-e = driver.find_elements_by_xpath('//*[@id="skills"]/div[2]/div[2]/a')
-if e:
-    e[0].click()
-list_of_skills_elements = driver.find_elements_by_class_name("dl-profile-skill-chip")
-lists_of_skills_names = [el.text for el in list_of_skills_elements]
-if looked_skill in lists_of_skills_names:
-    practicians_skills_shoulder.append(driver.current_url)
+# NUM_PAGES_TO_CRAWL = 100
+# for numPage in range(NUM_PAGES_TO_CRAWL):
+#     print(f"Crawling page number {numPage}...")
+#     for practician_xpath_index in range(len(list_of_practicians_xpath)):
+#         time.sleep(0.6)
+#         try:
+#             practician = driver.find_element_by_xpath(list_of_practicians_xpath[practician_xpath_index])
+#             practician.click()
+#             try:
+#                 e = driver.find_elements_by_xpath('//*[@id="skills"]/div[2]/div[2]/a')
+#                 e[0].click()
+#             except:
+#                 pass
+#             list_of_skills_elements = driver.find_elements_by_class_name("dl-profile-skill-chip")
+#             lists_of_skills_names = [el.text for el in list_of_skills_elements]
+#             if looked_skill in lists_of_skills_names:
+#                 practicians_skills_shoulder.append(driver.current_url)
+#             driver.back()
+#         except:
+#             driver.execute_script(f"window.scrollTo(0, {height-100});")
+#             height = driver.execute_script("return document.body.scrollHeight")
+#
+#         # reset height
+#         height = 4800
+#     # reset practician_xpath_index
+#     practician_xpath_index = 0
+
+for numPage in range(10):
+    print(f"Crawling page number {numPage}...")
+    for practician_xpath_index in range(len(list_of_practicians_xpath)):
+        time.sleep(1)
+        try:
+            practician = driver.find_element_by_xpath(list_of_practicians_xpath[practician_xpath_index])
+            practician.click()
+            try:
+                e = driver.find_elements_by_xpath('//*[@id="skills"]/div[2]/div[2]/a')
+                e[0].click()
+            except:
+                pass
+            list_of_skills_elements = driver.find_elements_by_class_name("dl-profile-skill-chip")
+            lists_of_skills_names = [el.text for el in list_of_skills_elements]
+            if looked_skill in lists_of_skills_names:
+                practicians_skills_shoulder.append(driver.current_url)
+            driver.back()
+        except:
+            driver.execute_script(f"window.scrollTo(0, {height - 100});")
+            #height = driver.execute_script("return document.body.scrollHeight")
+        # time.sleep(5)
+    # height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script(f"window.scrollTo(0, 3000);")
+    time.sleep(1)
+    next_page = driver.find_element_by_xpath('/html/body/div[7]/div/div[1]/div[11]/div[2]/a')
+    next_page.click()
+    # reset practician_xpath_index
+    practician_xpath_index = 0
+print(practicians_skills_shoulder)
+
+
+
+
+
+driver.quit()
+# practician_1_xpath = list_of_practicians_xpath[-1]
+# print(f"practician: {practician_1_xpath}")
+# practician = driver.find_element_by_xpath(practician_1_xpath)
+#
+# practician.click()
+# try:
+#     e = driver.find_elements_by_xpath('//*[@id="skills"]/div[2]/div[2]/a')
+#     e[0].click()
+# except:
+#     pass
+# # e = driver.find_elements_by_xpath('//*[@id="skills"]/div[2]/div[2]/a')
+# # if e:
+# #     e[0].click()
+#
+# list_of_skills_elements = driver.find_elements_by_class_name("dl-profile-skill-chip")
+# lists_of_skills_names = [el.text for el in list_of_skills_elements]
+# if looked_skill in lists_of_skills_names:
+#     practicians_skills_shoulder.append(driver.current_url)
 
 #
 # for practician in list_of_practicians:
@@ -125,3 +195,5 @@ if looked_skill in lists_of_skills_names:
 # e = driver.find_elements_by_xpath('//*[@id="skills"]/div[2]/div[2]/a')
 # if e:
 #     e[0].click()
+
+
